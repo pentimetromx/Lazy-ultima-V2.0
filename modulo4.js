1a40b95958480e6572eec71d4c92c50214a11287
juan mira <juanosvaldo7@yahoo.es>
2026-02-17 07:45:33 -0500

    mensaje del commit

--- modulo4.js
+++ modulo4.js
@@ -1921,11 +1921,11 @@ function ingresoEmpleado(){
 
   setTimeout(() => {
     if (!esDesktop) {
-      inputs.forEach(input => {
+      /* inputs.forEach(input => {
         if (input.tagName === 'INPUT') {
           input.readOnly = true;
         }
-      });      
+      }); */      
 
       calculadora.classList.remove('move-calculadora-1')
       calculadora.classList.remove('move-calculadora')
